2801171f096a988e209718cddcb299c3e712b6ba
Matt Seaton <mattseaton22@gmail.com>
2024-06-02 01:03:14 -0500

    Removed redundant script

--- gpu_uniforms/image/gpu_image.gd
+++ gpu_uniforms/image/gpu_image.gd
@@ -1,6 +1,6 @@
 # GLSL data type encoding: `image2D`
 extends GPUImageBase
-class_name GPU_Image
+class_name GPUImage
 
 const glsl_type = 'image2D'
 @export var data: Image = Image.create(1, 1, false, Image.FORMAT_RGBAF)
